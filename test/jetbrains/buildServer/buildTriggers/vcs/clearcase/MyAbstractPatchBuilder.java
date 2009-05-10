2d8d63caa7fa4a43c2605a053129f4fad56c06b8
unknown <gphilip@.dsk.fr.world.socgen>
2009-05-10 11:22:50 +0200

    Cleaned  up logs, cleatools commands are now in teamcity-vcs.log

--- test/jetbrains/buildServer/buildTriggers/vcs/clearcase/MyAbstractPatchBuilder.java
+++ test/jetbrains/buildServer/buildTriggers/vcs/clearcase/MyAbstractPatchBuilder.java
@@ -1,6 +1,7 @@
 package jetbrains.buildServer.buildTriggers.vcs.clearcase;
 
 import jetbrains.buildServer.vcs.AbstractPatchBuilder;
+import jetbrains.buildServer.log.Loggers;
 import org.apache.log4j.Logger;
 
 import java.io.File;
@@ -12,46 +13,59 @@ import java.io.InputStream;
  */
 class MyAbstractPatchBuilder extends AbstractPatchBuilder {
 
+  private int deleteFile, deleteDirectory, changesOrCreateTextFile,changeOrCreateBinaryFile,createDirectory ;
+
   private static final Logger LOG = Logger.getLogger(MyAbstractPatchBuilder.class);
 
   public void deleteFile(File file, boolean b) throws IOException {
-    LOG.info(String.format("ClearCaseSupportLiveTest.deleteFile , file=%s", file));
+    deleteFile++;
+    Loggers.VCS.debug(String.format("ClearCaseSupportLiveTest.deleteFile , file=%s", file));
   }
 
   public void deleteDirectory(File file, boolean b) throws IOException {
-    LOG.info(String.format("ClearCaseSupportLiveTest.deleteDirectory, file=%s", file));
+    deleteDirectory++;
+    Loggers.VCS.debug(String.format("ClearCaseSupportLiveTest.deleteDirectory, file=%s", file));
   }
 
   public void changeOrCreateTextFile(File file, String s, InputStream inputStream, long l, byte[] bytes)
       throws IOException {
-    LOG.info(String.format("changeOrCreateTextFile, file=%s, s=%s", file, s));
+    changesOrCreateTextFile++;
+    Loggers.VCS.debug(String.format("changeOrCreateTextFile, file=%s, s=%s", file, s));
   }
 
   public void changeOrCreateBinaryFile(File file, String s, InputStream inputStream, long l) throws IOException {
-    LOG.info(String.format("changeOrCreateBinaryFile, file=%s, s=%s", file, s));
+    changeOrCreateBinaryFile++;
+    Loggers.VCS.debug(String.format("changeOrCreateBinaryFile, file=%s, s=%s", file, s));
   }
 
   public void createDirectory(File file) throws IOException {
-    LOG.info(String.format("createDirectory, file=%s", file));
+    createDirectory++;
+    Loggers.VCS.debug(String.format("createDirectory, file=%s", file));
   }
 
   public void createBinaryFile(File file, String s, InputStream inputStream, long l) throws IOException {
-    LOG.info(String.format("createBinaryFile, file=%s", file));
+    Loggers.VCS.debug(String.format("createBinaryFile, file=%s", file));
   }
 
   public void createTextFile(File file, String s, InputStream inputStream, long l, byte[] bytes) throws IOException {
-    LOG.info(String.format("createTextFile, file=%s", file));
+    Loggers.VCS.debug(String.format("createTextFile, file=%s", file));
   }
 
   public void renameFile(File file, File file1, boolean b) throws IOException {
-    LOG.info(String.format("renameFile, file=%s, file1=%s", file, file1));
+    Loggers.VCS.debug(String.format("renameFile, file=%s, file1=%s", file, file1));
   }
 
   public void renameDirectory(File file, File file1, boolean b) throws IOException {
-    LOG.info(String.format("renameDirectory, file=%s, file1=%s", file, file1));
+    Loggers.VCS.debug(String.format("renameDirectory, file=%s, file1=%s", file, file1));
   }
 
   public void setLastModified(File currentRelativeFile, long time) throws IOException {
     throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public String toString() {
+    return String.format("MyAbstractPatchBuilder{deleteFile=%d, deleteDirectory=%d, changesOrCreateTextFile=%d, changeOrCreateBinaryFile=%d, createDirectory=%d'}'",
+        deleteFile, deleteDirectory, changesOrCreateTextFile, changeOrCreateBinaryFile, createDirectory);
   }
 }
