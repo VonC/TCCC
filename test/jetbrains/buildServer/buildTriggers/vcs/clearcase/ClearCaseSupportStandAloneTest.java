b2330038418686589771b0b6321593c73fd9f7ab
vonc <vonc@laposte.net>
2009-05-13 16:15:39 +0200

    empty test with testng launcher and setup log4j done

--- test/jetbrains/buildServer/buildTriggers/vcs/clearcase/ClearCaseSupportStandAloneTest.java
+++ test/jetbrains/buildServer/buildTriggers/vcs/clearcase/ClearCaseSupportStandAloneTest.java
@@ -3,10 +3,45 @@
  */
 package jetbrains.buildServer.buildTriggers.vcs.clearcase;
 
+import java.net.URL;
+
+import jetbrains.buildServer.log.Log4jFactory;
+
+import org.apache.log4j.xml.DOMConfigurator;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.intellij.openapi.diagnostic.Logger;
+
 /**
- * @author Vonc
- *
+ * Stand-alone tests for TeamCity ClearCase plugin. <br />
+ * Test BuildPatch and
+ * 
+ * @author <a href="mailto:vonc@laposte.net">VonC</a>
  */
+@Test
 public class ClearCaseSupportStandAloneTest {
+
+  private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(ClearCaseSupportStandAloneTest.class);
+  private ClearCaseSupport ccs;
+
+  @BeforeClass
+  public void setup() {
+    System.out.println("setup");
+    URL url = getClass().getResource("log4j.xml");
+    DOMConfigurator.configure(url);
+    Logger.setFactory(new Log4jFactory());
+    ccs = new ClearCaseSupport();
+  }
+
+  public void test() {
+    System.out.println("test");
+  }
+
+  @AfterClass
+  public void teardown() {
+    System.out.println("teardown");
+  }
 
 }
